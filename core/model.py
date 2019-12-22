db962de0a94d3aab618918f3a9848878e2d3ad41
VIPranowo <33710222+VIPranowo@users.noreply.github.com>
2019-12-22 19:21:32 +0700

    Update model.py

--- core/model.py
+++ core/model.py
@@ -90,7 +90,8 @@ class Model():
 		#Predict each timestep given the last sequence of true data, in effect only predicting 1 step ahead each time
 		print('[Model] Predicting Point-by-Point...')
 		predicted = self.model.predict(data)
-		predicted = np.reshape(predicted, (predicted.size,))
+		# predicted2 = np.reshape(predicted, (predicted.size,))
+		# print(predicted.size)
 		return predicted
 
 	def predict_sequences_multiple(self, data, window_size, prediction_len):
